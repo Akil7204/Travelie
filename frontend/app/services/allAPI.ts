fe0832a9852001de6b34e760c8565d362fdb4781
Akil <akilakil6874@gmail.com>
2024-08-14 10:08:13 +0530

    services folder created and added API and server url

---
+++ frontend/app/services/allAPI.ts
@@ -0,0 +1,24 @@
+import { commonAPI } from "./commonAPI";
+import { SERVER_URL } from "./serverURL";
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `${SERVER_URL}/api/users`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// SignUp API
+export const SignUpAPI = async (
+  reqBody: any,
+  reqHeader: { "Content-Type": string } | undefined
+) => {
+  return await commonAPI("POST", `${SERVER_URL}/signup`, reqBody, reqHeader);
+};
+
+// Login API
+export const LoginAPI = async (reqBody: any) => {
+  return await commonAPI("POST", `${SERVER_URL}/login`, reqBody);
+};
---
+++ frontend/app/services/serverURL.ts
@@ -0,0 +1,2 @@
+export const SERVER_URL = "http://localhost:4000/api/users";
+export const SERVER_URL_COMPANY = "http://localhost:4000/api/company";
