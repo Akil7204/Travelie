2a0f47647ffdff7ca1860228391d9fab64c01952
Akil <akilakil6874@gmail.com>
2024-08-18 19:06:39 +0530

    bug fixed in approval page

--- frontend/app/admin/approval/page.tsx
+++ frontend/app/admin/approval/page.tsx
@@ -23,11 +23,11 @@ const CompanyApproval: React.FC = () => {
   useEffect(() => {
     const fetchCompanies = async () => {
       try {
-        const token = localStorage.getItem('adminToken');
-        if (!token) throw new Error('No token found');
+        const token = localStorage.getItem("adminToken");
+        if (!token) throw new Error("No token found");
         const response = await getAllUnapprovalAPI(token);
 
-  console.log({response});
+        console.log({ response });
         setCompanies(response); // Assuming the API response is in the correct format
       } catch (error) {
         console.error("Failed to fetch unapproved companies:", error);
@@ -38,7 +38,6 @@ const CompanyApproval: React.FC = () => {
   }, []);
 
   const handleApprove = (id: string) => {
-    
     setCompanies(companies.filter((company) => company._id !== id));
     sendEmail(id, "Approved");
   };
@@ -77,51 +76,52 @@ const CompanyApproval: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-              { companies &&  companies.map((company) => (
-              <tr key={company._id}>
-                <td className="py-3 px-4">{company._id}</td>
-                <td className="py-3 px-4">{company.companyname}</td>
-                <td className="py-3 px-4">{company.email}</td>
-                <td className="py-3 px-4">{company.phone}</td>
-                <td className="py-3 px-4">
-                  <span
-                    className={`px-2 py-1 rounded ${
-                      company.adminVerified === false
-                        ? "bg-gray-300"
-                        : company.adminVerified === true
-                        ? "bg-green-300"
-                        : "bg-red-300"
-                    }`}
-                  >
-                    Pending
-                  </span>
-                </td>
-                <td className="py-3 px-4 flex space-x-2">
-                  {company.adminVerified === false && (
-                    <>
-                      <button
-                        onClick={() => handleReject(company._id)}
-                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        onClick={() => handleApprove(company._id)}
-                        className="bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600"
-                      >
-                        Approval
-                      </button>
-                    </>
-                  )}
-                  {/* {company.status === "Rejected" && (
+            {companies &&
+              companies.map((company) => (
+                <tr key={company._id}>
+                  <td className="py-3 px-4">{company._id}</td>
+                  <td className="py-3 px-4">{company.companyname}</td>
+                  <td className="py-3 px-4">{company.email}</td>
+                  <td className="py-3 px-4">{company.phone}</td>
+                  <td className="py-3 px-4">
+                    <span
+                      className={`px-2 py-1 rounded ${
+                        company.adminVerified === false
+                          ? "bg-gray-300"
+                          : company.adminVerified === true
+                          ? "bg-green-300"
+                          : "bg-red-300"
+                      }`}
+                    >
+                      Pending
+                    </span>
+                  </td>
+                  <td className="py-3 px-4 flex space-x-2">
+                    {company.adminVerified === false && (
+                      <>
+                        <button
+                          onClick={() => handleReject(company._id)}
+                          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          onClick={() => handleApprove(company._id)}
+                          className="bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600"
+                        >
+                          Approval
+                        </button>
+                      </>
+                    )}
+                    {/* {company.status === "Rejected" && (
                     <span className="text-red-500">Canceled</span>
                   )}
                   {company.status === "Approved" && (
                     <span className="text-green-500">Approved</span>
                   )} */}
-                </td>
-              </tr>
-            ))}
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
