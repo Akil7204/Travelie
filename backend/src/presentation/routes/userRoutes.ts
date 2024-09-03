e763cbfcf74af5a962e5c6cf35c796318ca7ff59
Akil <akilakil6874@gmail.com>
2024-09-03 23:07:24 +0530

    middleware completed in user side

--- backend/src/presentation/routes/userRoutes.ts
+++ backend/src/presentation/routes/userRoutes.ts
@@ -1,6 +1,13 @@
-import express  from "express";
-import { getAllTrips, googleLoginHandler, login, register, verifyOtp } from "../controllers/userController";
-
+import express from "express";
+import {
+  getAllTrips,
+  googleLoginHandler,
+  login,
+  register,
+  verifyOtp,
+} from "../controllers/userController";
+import upload from "../../uilts/multer";
+import { verifyUser } from "../MiddleWare/userJWT";
 
 const router = express.Router();
 
@@ -11,6 +18,13 @@ router.post("/verifyOtp", verifyOtp);
 router.post("/login", login);
 
 router.post("/googleLogin", googleLoginHandler); // googleLoginHandler
+
+router.put(
+  "/profile",
+  verifyUser,
+  upload.single("profileImage")
+  //   updateProfile
+);
 
 router.get("/trips", getAllTrips);
 
