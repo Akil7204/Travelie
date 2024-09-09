7c8fc0c1c39d1804583e90c1c65438223c53b084
Akil <akilakil6874@gmail.com>
2024-09-09 14:03:34 +0530

    loader added in trip page and trip detail page

--- frontend/app/discover/[id]/page.tsx
+++ frontend/app/discover/[id]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Image from "next/image";
 import Navbar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { useParams } from "next/navigation";
 import { detailTripsAPI } from "@/app/services/allAPI";
+import { DNA, InfinitySpin } from "react-loader-spinner";
 
 interface Trip {
   _id: string;
@@ -15,8 +16,8 @@ interface Trip {
   days: number;
   startingFrom: string;
   endingAt: string;
-  startingDate: Date;
-  endingDate: Date;
+  startingDate: any;
+  endingDate: any;
   price: number;
   locations: string[];
   status: string;
@@ -27,8 +28,8 @@ interface Trip {
 }
 
 const TripPage: React.FC = () => {
-  const [trips, setTrips] = useState<Trip[]>([]);
-
+  const [tripData, setTripData] = useState<Trip | undefined>();
+  const [loading, setLoading] = useState<boolean>(true);
   const params = useParams(); // Access the dynamic parameters
   const tripId: any = params.id;
 
@@ -38,8 +39,11 @@ const TripPage: React.FC = () => {
         const response = await detailTripsAPI(tripId); // Fetch trips from API
         console.log("Detail API:", response); // Log API response
 
-        setTrips(response); // Update state with fetched trips
+        setTripData(response); // Update state with fetched trips;
+        setLoading(false);
+        // tripData.endingDate = formatDateToDDMMYYYY(tripData?.endingDate)
       } catch (error) {
+        setLoading(false);
         console.error("Failed to fetch trips:", error);
       }
     };
@@ -51,111 +55,118 @@ const TripPage: React.FC = () => {
     <>
       <Navbar />
       <div className="max-w-5xl mx-auto p-6 pt-14">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-3">
-          <div className="col-span-1">
-            <Image
-              src="/img/GettyImages-483645840-5a06b125da27150037f954d1.jpg" // Replace with your main image path
-              alt="Varkala"
-              width={800}
-              height={400}
-              className="rounded-md h-[450px] object-cover transition-all duration-200"
+        {loading ? ( // Show loader while loading is true
+          <div className="flex justify-center items-center min-h-[500px]">
+            <DNA
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="dna-loading"
+              wrapperStyle={{}}
+              wrapperClass="dna-wrapper"
             />
           </div>
-          <div className="col-span-1">
-            <div className="grid grid-rows-4 gap-4">
-              <Image
-                src="/img/GettyImages-483645840-5a06b125da27150037f954d1.jpg" // Replace with your image path
-                alt="Varkala Beach"
-                width={300}
-                height={200}
-                className="rounded-md h-[100px] object-cover cursor-pointer transition-all duration-700"
-              />
-              <Image
-                src="/img/GettyImages-483645840-5a06b125da27150037f954d1.jpg" // Replace with your image path
-                alt="Varkala Cliff"
-                width={300}
-                height={200}
-                className="rounded-md h-[100px] object-cover cursor-pointer transition-all duration-700"
-              />
-              <Image
-                src="/img/GettyImages-483645840-5a06b125da27150037f954d1.jpg" // Replace with your image path
-                alt="Varkala Market"
-                width={300}
-                height={200}
-                className="rounded-md h-[100px] object-cover cursor-pointer transition-all duration-700"
-              />
+        ) : (
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-3">
+              {/* Main Image */}
+              <div className="col-span-1">
+                {tripData?.images[0] && (
+                  <Image
+                    src={tripData?.images[0]} // Main image from the trip data
+                    alt="image1"
+                    width={800}
+                    height={400}
+                    className="rounded-md h-[450px] object-cover transition-all duration-200"
+                  />
+                )}
+              </div>
+
+              {/* Small Images */}
+              <div className="col-span-1">
+                <div className="grid grid-rows-4 gap-4">
+                  {tripData?.images?.map((image: string, index: number) => (
+                    <Image
+                      key={index}
+                      src={image}
+                      alt={`Image ${index + 1}`}
+                      width={300}
+                      height={200}
+                      className="rounded-md h-[100px] object-cover cursor-pointer transition-all duration-700"
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="mt-8">
-          <h1 className="text-4xl font-bold text-blue-600 capitalize">
-            Varkala
-          </h1>
-          <p className="text-gray-600">
-            <span className="text-yellow-500">★ 4.5</span> (1200 Reviews)
-          </p>
-          <p className="text-gray-600 mt-2">
-            <i className="fas fa-map-marker-alt"></i> Lorem ipsum road, Tantri
-            nagar, 2322, Varkala, Kerala
-          </p>
-        </div>
+            <div className="mt-8">
+              {tripData?.tripName && (
+                <h1 className="text-4xl font-bold text-blue-600 capitalize">
+                  {tripData.tripName}
+                </h1>
+              )}
+              <p className="text-gray-600 mt-2">
+                <i className="fas fa-map-marker-alt"></i> {tripData?.tripName}
+              </p>
+            </div>
 
-        <div className="mt-6">
-          <h2 className="text-2xl font-semibold">Overview</h2>
-          <p className="text-gray-700 mt-2">
-            Varkala Beach, also known as Papanasam Beach, is situated in the
-            town of Varkala. It’s part of the Indian Ocean and known for its
-            cliffs and serene beauty.
-          </p>
-        </div>
+            <div className="mt-6">
+              <h2 className="text-2xl font-semibold">Overview</h2>
+              <p className="text-gray-700 mt-2">{tripData?.description}</p>
+            </div>
 
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
-            <h3 className="text-xl font-semibold">Trip Details</h3>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
-              <li>
-                <strong>From:</strong> Palakkad Fort
-              </li>
-              <li>
-                <strong>To:</strong> Varkala
-              </li>
-              <li>
-                <strong>Duration:</strong> 3 Days
-              </li>
-              <li>
-                <strong>Vehicle:</strong> Bus
-              </li>
-              <li>
-                <strong>Start Date:</strong> 04-Aug-2024
-              </li>
-              <li>
-                <strong>Return Date:</strong> 06-Aug-2024
-              </li>
-              <li>
-                <strong>Time:</strong> 3:00 PM
-              </li>
-            </ul>
-          </div>
+            <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div>
+                <h3 className="text-xl font-semibold">Trip Details</h3>
+                <ul className="list-disc list-inside text-gray-700 mt-2">
+                  <li>
+                    <strong>From:</strong> {tripData?.startingFrom}
+                  </li>
+                  <li>
+                    <strong>To:</strong> {tripData?.tripName}
+                  </li>
+                  <li>
+                    <strong>No. of Days:</strong> {tripData?.days}
+                  </li>
+                  <li>
+                    <strong>Vehicle:</strong> {tripData?.category}
+                  </li>
+                  <li>
+                    <strong>Start Date:</strong>{" "}
+                    {new Date(tripData?.startingDate).toLocaleDateString()}
+                  </li>
+                  <li>
+                    <strong>Return Date:</strong>{" "}
+                    {new Date(tripData?.endingDate).toLocaleDateString()}
+                  </li>
+                  {tripData && (
+                    <li>
+                      <strong>Available Seats:</strong>{" "}
+                      {tripData?.seats - (tripData?.bookedSeats || 0) === 0
+                        ? "Seat full"
+                        : tripData?.seats - (tripData?.bookedSeats || 0)}
+                    </li>
+                  )}
+                </ul>
+              </div>
 
-          <div>
-            <h3 className="text-xl font-semibold">Things to See & Do</h3>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
-              <li>Varkala Beach</li>
-              <li>Kappil Lake</li>
-              <li>Anjengo Fort</li>
-              <li>Varkala Fort</li>
-              <li>Ponnumthuruthu Island</li>
-              <li>Thiruvambadi Beach</li>
-            </ul>
-          </div>
-        </div>
+              <div>
+                <h3 className="text-xl font-semibold">Things to See & Do</h3>
+                <ul className="list-disc list-inside text-gray-700 mt-2">
+                  {tripData?.locations.map((thing: string, index: number) => (
+                    <li key={index}>{thing}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
 
-        <div className="mt-8">
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
-            Book
-          </button>
-        </div>
+            <div className="mt-8">
+              <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+                Book
+              </button>
+            </div>
+          </>
+        )}
       </div>
       <Footer />
     </>
--- frontend/components/page/TripSection.tsx
+++ frontend/components/page/TripSection.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { allTripsAPI } from "@/app/services/allAPI";
-
+import { DNA } from "react-loader-spinner";
 
 interface Trip {
   _id: string;
@@ -27,6 +27,7 @@ interface Trip {
 const TripSection: React.FC = () => {
   const [trips, setTrips] = useState<Trip[]>([]);
   const [formattedDates, setFormattedDates] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchTrips = async () => {
@@ -35,7 +36,9 @@ const TripSection: React.FC = () => {
         console.log("API Response:", response); // Log API response
 
         setTrips(response); // Update state with fetched trips
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
         console.error("Failed to fetch trips:", error);
       }
     };
@@ -58,47 +61,62 @@ const TripSection: React.FC = () => {
   }, [trips]);
 
   return (
-    <section className="w-full md:w-3/4 p-4">
-      <h2 className="text-2xl font-bold text-gray-900 mb-6">
-        Best Packages For You
-      </h2>
-      <div className="space-y-6">
-        {trips.map((trip, index) => (
-          <div
-            key={index}
-            className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col md:flex-row h-64"
-          >
-            <div className="h-full w-60 md:w-1/5 flex-shrink-0">
-              <img
-                className="h-full w-full object-cover"
-                src={trip.images[0]}
-                alt={trip.tripName}
-              />
-            </div>
-            <div className="p-4 flex-grow flex flex-col justify-between">
-              <div>
-                <h3 className="text-lg font-semibold text-gray-800">
-                  {trip.tripName}
-                </h3>
-                <p className="text-gray-600 mt-2">{trip.description}</p>
-                <div className="flex items-center mt-4 space-x-4">
-                  <p className="text-black mt-2">Per head /</p>
-                  <span className="text-green-500 mt-2">
-                    Rs. {trip.price}
-                  </span>
+    <>
+      {loading ? ( // Show loader while loading is true
+        <div className="flex justify-center items-center h-screen w-full">
+          <DNA
+            visible={true}
+            height="100"
+            width="100"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        </div>
+      ) : (
+        <section className="w-full md:w-3/4 p-4">
+          <h2 className="text-2xl font-bold text-gray-900 mb-6">
+            Best Packages For You
+          </h2>
+          <div className="space-y-6">
+            {trips.map((trip, index) => (
+              <div
+                key={index}
+                className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col md:flex-row h-64"
+              >
+                <div className="h-full w-60 md:w-1/5 flex-shrink-0">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={trip.images[0]}
+                    alt={trip.tripName}
+                  />
+                </div>
+                <div className="p-4 flex-grow flex flex-col justify-between">
+                  <div>
+                    <h3 className="text-lg font-semibold text-gray-800">
+                      {trip.tripName}
+                    </h3>
+                    <p className="text-gray-600 mt-2">{trip.description}</p>
+                    <div className="flex items-center mt-4 space-x-4">
+                      <p className="text-black mt-2">Per head /</p>
+                      <span className="text-green-500 mt-2">
+                        Rs. {trip.price}
+                      </span>
+                    </div>
+                  </div>
+                  <button className="mt-4 px-4 py-2 w-40 bg-blue-500 text-white rounded-lg">
+                    <a href={`/discover/${trip._id}`}>See availability</a>
+                  </button>
                 </div>
               </div>
-              <button className="mt-4 px-4 py-2 w-40 bg-blue-500 text-white rounded-lg">
-                <a href={`/discover/${trip._id}`}>See availability</a>
-              </button>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <button className="mt-8 px-4 py-2 bg-gray-200 text-gray-800 rounded-lg">
-        Load more results
-      </button>
-    </section>
+          <button className="mt-8 px-4 py-2 bg-gray-200 text-gray-800 rounded-lg">
+            Load more results
+          </button>
+        </section>
+      )}
+    </>
   );
 };
 
