77ca4a92851780b36fe023db6146558dc733fb49
Akil <akilakil6874@gmail.com>
2024-10-06 22:14:49 +0530

    bug fixing

--- frontend/utils/constants.ts
+++ frontend/utils/constants.ts
@@ -1,21 +1,18 @@
 export const PayU = {
-    merchantKey: 'PMd9OW',
-  };
-  
-  //  /* PROD: =
-  export const FRONTEND_DOMAIN = "https://travelie.life";
-  
-  const DOMAIN = "https://travelie.life";
-  
-  const USER_SERVICE_DOMAIN = DOMAIN;
-  const POSTS_SERVICE_DOMAIN = DOMAIN;
-  const MESSAGE_SERVICE_DOMAIN = DOMAIN;
-  const NOTIFICATION_SERVICE_DOMAIN = DOMAIN;
-  export const PAYMENT_SERVICE_DOMAIN = DOMAIN;
+  merchantKey: "PMd9OW",
+};
 
-  
-  
-  export const SOCKET_URI = "https://travelie.life";
-  
-  
-  export const JWT_SECRET =process.env.JWT_SECRET
+//  /* PROD: =
+export const FRONTEND_DOMAIN = "https://travelie.life";
+
+const DOMAIN = "https://travelie.life";
+
+const USER_SERVICE_DOMAIN = DOMAIN;
+const POSTS_SERVICE_DOMAIN = DOMAIN;
+const MESSAGE_SERVICE_DOMAIN = DOMAIN;
+const NOTIFICATION_SERVICE_DOMAIN = DOMAIN;
+export const PAYMENT_SERVICE_DOMAIN = DOMAIN;
+
+export const SOCKET_URI = "https://travelie.life";
+
+export const JWT_SECRET = process.env.JWT_SECRET;
