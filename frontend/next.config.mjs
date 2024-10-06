5868f9fd85e71c66f19df99ab73fa2ab1b1eed4a
Akil <akilakil6874@gmail.com>
2024-10-06 23:27:11 +0530

    next config updated

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,20 +7,25 @@ const nextConfig = {
         "travelie-images.s3.ap-south-1.amazonaws.com",
       ],
     },
-    async headers() {
-      return [
-          {
-              // matching all API routes
-              source: "/api/:path*",
-              headers: [
-                  { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-              ]
-          }
-      ]
-  }
+    // async headers() {
+    //   return [
+    //       {
+    //           // matching all API routes
+    //           source: "/api/:path*",
+    //           headers: [
+    //               { key: "Access-Control-Allow-Credentials", value: "true" },
+    //               { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+    //               { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+    //               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    //           ]
+    //       }
+    //   ]
+  // }
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['travelie.life', '*.travelie.life', 'test.payu.in'],
+    },
+  },
 };
 
 export default nextConfig;
