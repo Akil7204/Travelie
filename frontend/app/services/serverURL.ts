452f22729ce2deafcee36a8adc3d6b2fe2ef448f
Akil <akilakil6874@gmail.com>
2024-10-06 23:37:30 +0530

    bug fixing

--- frontend/app/services/serverURL.ts
+++ frontend/app/services/serverURL.ts
@@ -1,6 +1,6 @@
-export const SERVER_URL = "https://travelie.life/api/v1/users";
-export const SERVER_URL_COMPANY = "https://travelie.life/api/v1/company";
-export const server_URL_admin = "https://travelie.life/api/v1/admin";
-export const SERVER_URL_CHAT = "https://travelie.life/api/v1/chat"
-export const SERVER_URL_MESSAGE = "https://travelie.life/api/v1/message";
-export const SERVER_URL_ISBLOCKED = "https://travelie.life/api/v1";
+export const SERVER_URL = "https://travelie.life/v1/api/users";
+export const SERVER_URL_COMPANY = "https://travelie.life/v1/api/company";
+export const server_URL_admin = "https://travelie.life/v1/api/admin";
+export const SERVER_URL_CHAT = "https://travelie.life/v1/api/chat"
+export const SERVER_URL_MESSAGE = "https://travelie.life/v1/api/message";
+export const SERVER_URL_ISBLOCKED = "https://travelie.life/v1/api";
