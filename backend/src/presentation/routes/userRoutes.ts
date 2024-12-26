8f8186bd364a5b5786006d67bfb65f03317ba794
Akil <akilakil6874@gmail.com>
2024-12-26 18:55:31 +0530

    bug fixing

--- backend/src/presentation/routes/userRoutes.ts
+++ backend/src/presentation/routes/userRoutes.ts
@@ -30,7 +30,7 @@ router.post("/verifyOtp", verifyOtp);
 
 router.post("/login", login);
 
-router.post("/googleLogin", googleLoginHandler); // googleLoginHandler
+router.post("/googleLogin", googleLoginHandler); 
 
 router.put(
   "/profile",
