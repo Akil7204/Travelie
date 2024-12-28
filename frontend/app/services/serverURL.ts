f4ade5ef2e5c48e6ba3f2f6005dbcf8940976f06
Akil <akilakil6874@gmail.com>
2024-12-28 21:29:15 +0530

    connecting backend and frontend

--- frontend/app/services/serverURL.ts
+++ frontend/app/services/serverURL.ts
@@ -1,9 +1,9 @@
-export const SERVER_URL = "https://travelie.life/v1/api/users";
-export const SERVER_URL_COMPANY = "https://travelie.life/v1/api/company";
-export const server_URL_admin = "https://travelie.life/v1/api/admin";
-export const SERVER_URL_CHAT = "https://travelie.life/v1/api/chat"
-export const SERVER_URL_MESSAGE = "https://travelie.life/v1/api/message";
-export const SERVER_URL_ISBLOCKED = "https://travelie.life/v1/api";
+// export const SERVER_URL = "https://travelie.life/v1/api/users";
+// export const SERVER_URL_COMPANY = "https://travelie.life/v1/api/company";
+// export const server_URL_admin = "https://travelie.life/v1/api/admin";
+// export const SERVER_URL_CHAT = "https://travelie.life/v1/api/chat"
+// export const SERVER_URL_MESSAGE = "https://travelie.life/v1/api/message";
+// export const SERVER_URL_ISBLOCKED = "https://travelie.life/v1/api";
 
 // export const SERVER_URL = "http://localhost:4000/v1/api/users";
 // export const SERVER_URL_COMPANY = "http://localhost:4000/v1/api/company";
@@ -11,3 +11,10 @@ export const SERVER_URL_ISBLOCKED = "https://travelie.life/v1/api";
 // export const SERVER_URL_CHAT = "http://localhost:4000/v1/api/chat"
 // export const SERVER_URL_MESSAGE = "http://localhost:4000/v1/api/message";
 // export const SERVER_URL_ISBLOCKED = "http://localhost:4000/v1/api";
+
+export const SERVER_URL = "https://travelie.onrender.com/v1/api/users";
+export const SERVER_URL_COMPANY = "https://travelie.onrender.com/v1/api/company";
+export const server_URL_admin = "https://travelie.onrender.com/v1/api/admin";
+export const SERVER_URL_CHAT = "https://travelie.onrender.com/v1/api/chat"
+export const SERVER_URL_MESSAGE = "https://travelie.onrender.com/v1/api/message";
+export const SERVER_URL_ISBLOCKED = "https://travelie.onrender.com/v1/api";
