d6db8924d7600302509a1e7c5f58364d4cfd8ce7
Akil <akilakil6874@gmail.com>
2025-02-04 16:55:00 +0530

    bug fixed in frontend serverUrl page

--- frontend/app/services/serverURL.ts
+++ frontend/app/services/serverURL.ts
@@ -12,9 +12,9 @@
 // export const SERVER_URL_MESSAGE = "http://localhost:4000/v1/api/message";
 // export const SERVER_URL_ISBLOCKED = "http://localhost:4000/v1/api";
 
-export const SERVER_URL = "https://api.travelie.onrender.com/v1/api/users";
-export const SERVER_URL_COMPANY = "https://api.travelie.onrender.com/v1/api/company";
-export const server_URL_admin = "https://api.travelie.onrender.com/v1/api/admin";
-export const SERVER_URL_CHAT = "https://api.travelie.onrender.com/v1/api/chat"
-export const SERVER_URL_MESSAGE = "https://api.travelie.onrender.com/v1/api/message";
-export const SERVER_URL_ISBLOCKED = "https://api.travelie.onrender.com/v1/api";
+export const SERVER_URL = "https://api.travelie.life/v1/api/users";
+export const SERVER_URL_COMPANY = "https://api.travelie.life/v1/api/company";
+export const server_URL_admin = "https://api.travelie.life/v1/api/admin";
+export const SERVER_URL_CHAT = "https://api.travelie.life/v1/api/chat"
+export const SERVER_URL_MESSAGE = "https://api.travelie.life/v1/api/message";
+export const SERVER_URL_ISBLOCKED = "https://api.travelie.life/v1/api";
